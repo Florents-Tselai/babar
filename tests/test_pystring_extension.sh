aebb66923bc3a1a79b1c983a2f02b4fc3ff1171f
Florents Tselai <florents.tselai@gmail.com>
2023-11-25 18:10:32 +0200

    Remove extension install from test workflow

--- tests/test_pystring_extension.sh
+++ tests/test_pystring_extension.sh
@@ -3,10 +3,10 @@
 python ./babar/examples/pystring.py
 make install
 
-#psql -d postgres <<SQL
-#DROP EXTENSION IF EXISTS pystring;
-#CREATE EXTENSION pystring;
-#
-#select pyjoin(array ['a', 'b'], ', ');
-#
-#SQL
+psql -d postgres <<SQL
+DROP EXTENSION IF EXISTS pystring;
+CREATE EXTENSION pystring;
+
+select pyjoin(array ['a', 'b'], ', ');
+
+SQL
