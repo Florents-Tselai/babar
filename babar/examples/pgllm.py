efaf2fcd915daa06fee1ad23402bef2513245b0c
Florents Tselai <florents.tselai@gmail.com>
2023-11-30 11:49:16 +0200

    llm example

--- babar/examples/pgllm.py
+++ babar/examples/pgllm.py
@@ -1,14 +1,20 @@
 from babar import Extension
 from typing import List
 
-try:
-    import llm
-except ImportError:
-    raise Exception(
-        "You should `pip install llm`"
-        "and probably some more models like `llm install llm-clip` . "
-        "See https://llm.datasette.io/en/stable/plugins/directory.html#local-models"
-    )
+
+def generate_text() -> List[str]:
+    return [
+        "SnakesOnAPlane: A package for handling unexpected errors in Python with style.",
+        "PandasInPajamas: A playful twist on the popular Pandas package, for data analysis in comfort.",
+        "PyJokes: A simple library that generates random programming jokes.",
+        "FizzBuzzWhizz: A fun package for playing the classic FizzBuzz game with a twist.",
+        "NinjaTurtles: A graphics package for drawing turtle graphics with a ninja twist.",
+        "PythonicPotion: A magical package for creating enchanting Python scripts.",
+        "CodeMonkey: A package for automating repetitive coding tasks, with a cheeky monkey theme.",
+        "DjangoUnchained: A Django extension for more freedom and less boilerplate code.",
+        "MontyPython: A humorous package filled with references and jokes from the Monty Python series.",
+        "GigaBytesAndGiggles: A package for visualizing and joking about big data.",
+    ]
 
 
 def llm_embed(text: str, model: str = "clip") -> List[float]:
@@ -23,6 +29,7 @@ if __name__ == "__main__":
     Extension(
         "pgllm",
         llm_embed,
+        generate_text,
         comment="LLMs in postgres powered by Python LLM",
         default_version="0.1.0",
     )
